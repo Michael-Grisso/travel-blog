ea4882180a1bb236a550e14f34a5991fdfb89f8e
Michael <michaelgrisso2011@gmail.com>
2022-10-22 13:18:22 -0400

    added class

---
+++ controllers/locations.js
@@ -0,0 +1,9 @@
+class Locations {
+  constructor(id = 0, location, description, image, experience) {
+    this.id = id;
+    this.location = location;
+    this.description = description;
+    this.image = image;
+    this.experience = experience;
+  }
+}
